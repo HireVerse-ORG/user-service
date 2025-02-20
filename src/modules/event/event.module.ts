758df01d2a62b9a11274ebc2be201326953564de
rahiii-dev <rahiofcl@gmail.com>
2025-02-21 01:35:50 +0530

    fix: kafka retry

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -3,17 +3,25 @@ import { kafka } from "@hireverse/kafka-communication";
 import TYPES from "../../core/container/container.types";
 import { KafkaConnect, KafkaProducer } from "@hireverse/kafka-communication/dist/kafka";
 import { EventService } from "./event.service";
+import { logger } from "../../core/utils/logger";
 
 const kafkaConnect = new KafkaConnect({
     clientId: "user-service",
     brokers: [process.env.KAFKA_SERVER!],
     retry: {
-        retries: 5, 
-        factor: 0.2,
+        retries: 10,              
+        initialRetryTime: 500,   
+        factor: 0.3,              
+        multiplier: 2,           
+        maxRetryTime: 60_000,    
+        restartOnFailure: async (error) => {
+            logger.error("Kafka connection failed:", error);
+            return true; 
+        },
     }
 })
 
-export const Producer = new kafka.KafkaProducer(kafkaConnect, {allowAutoTopicCreation: process.env.NODE_ENV === "development"});
+export const Producer = new kafka.KafkaProducer(kafkaConnect, {allowAutoTopicCreation: true});
 
 export function loadEventContainer(container: Container) {
     container.bind<KafkaProducer>(TYPES.KafkaProducer).toConstantValue(Producer);
